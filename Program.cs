ebec40fbfcf781cf765b53be5ade691c99153661
Jim.Binder <Jim1720@aol.com>
2022-03-30 13:43:33 -0700

    Historical Features

--- Program.cs
+++ Program.cs
@@ -18,13 +18,24 @@ namespace A70Insurance
         public static async Task Main(string[] args)
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
-            builder.RootComponents.Add<App>("app");
+            builder.RootComponents.Add<App>("#app");
 
-            builder.Services.AddBlazoredSessionStorage(); 
+            builder.Services.AddBlazoredSessionStorage();
 
             Env.url = builder.Configuration.GetValue<String>("UrlPrefix");
 
             Env.usingStyles = builder.Configuration.GetValue<String>("UseStyles");
+
+            const string yes = "Y";
+
+            Env.usingStay = builder.Configuration.GetValue<string>("UseStay") == yes;
+
+            Env.usingFocus = builder.Configuration.GetValue<string>("UseFocus") == yes;
+
+            Env.usingNav = builder.Configuration.GetValue<string>("UseNav") == yes;
+
+            Env.usingActions = builder.Configuration.GetValue<string>("UseActions") == yes;
+
 
             builder.Services.AddScoped<IScreenStyleFactory, ScreenStyleFactory>();
 
@@ -33,6 +44,13 @@ namespace A70Insurance
             builder.Services.AddScoped<IScreenStyleManager, ScreenStyleManager>();
 
             builder.Services.AddScoped<IEditDate, EditDate>();
+
+            builder.Services.AddScoped<IActionOperations, ActionOperations>();
+
+            builder.Services.AddScoped<IFocusedClaim, FocusedClaim>();
+
+            builder.Services.AddScoped<IHistorySettings, HistorySettings>();
+
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
