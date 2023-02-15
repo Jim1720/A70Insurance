b857b631809365bc2ac4a04741748144c39c7d49
Jim.Binder <Jim1720@aol.com>
2023-02-15 15:19:38 -0800

    net 6 upgrade bootstrap 5

--- Program.cs
+++ Program.cs
@@ -1,60 +1,54 @@
+using A70Insurance;
 using A70Insurance.Models;
 using A70Insurance.StyleFeature;
 using Blazored.SessionStorage;
+using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Net.Http;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace A70Insurance
-{
-    public class Program
-    {
-        public static async Task Main(string[] args)
-        {
-            var builder = WebAssemblyHostBuilder.CreateDefault(args);
-            builder.RootComponents.Add<App>("#app");
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
 
-            builder.Services.AddBlazoredSessionStorage();
-
-            Env.url = builder.Configuration.GetValue<String>("UrlPrefix");
-
-            Env.usingStyles = builder.Configuration.GetValue<String>("UseStyles");
-
-            const string yes = "Y";
-
-            Env.usingStay = builder.Configuration.GetValue<string>("UseStay") == yes;
-
-            Env.usingFocus = builder.Configuration.GetValue<string>("UseFocus") == yes;
-
-            Env.usingNav = builder.Configuration.GetValue<string>("UseNav") == yes;
-
-            Env.usingActions = builder.Configuration.GetValue<string>("UseActions") == yes;
+builder.Services.AddBlazoredSessionStorage();
 
 
-            builder.Services.AddScoped<IScreenStyleFactory, ScreenStyleFactory>();
+// ***************************************************************
+// Values come from app settings json NOT environment variables 
+// ***************************************************************
 
-            builder.Services.AddScoped<IScreenStyleList, ScreenStyleList>();
+Env.url = builder.Configuration.GetValue<String>("UrlPrefix"); 
 
-            builder.Services.AddScoped<IScreenStyleManager, ScreenStyleManager>();
+Env.usingStyles = builder.Configuration.GetValue<String>("UseStyles");
 
-            builder.Services.AddScoped<IEditDate, EditDate>();
+Env.ePattern = builder.Configuration.GetValue<string>("ePattern");
 
-            builder.Services.AddScoped<IActionOperations, ActionOperations>();
+const string yes = "Y";
 
-            builder.Services.AddScoped<IFocusedClaim, FocusedClaim>();
+Env.usingStay = builder.Configuration.GetValue<string>("UseStay") == yes;
 
-            builder.Services.AddScoped<IHistorySettings, HistorySettings>();
+Env.usingFocus = builder.Configuration.GetValue<string>("UseFocus") == yes;
+
+Env.usingNav = builder.Configuration.GetValue<string>("UseNav") == yes;
+
+Env.usingActions = builder.Configuration.GetValue<string>("UseActions") == yes;
+ 
 
 
-            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped<IScreenStyleFactory, ScreenStyleFactory>();
 
-            await builder.Build().RunAsync();
-        }
-    }
-}
+builder.Services.AddScoped<IScreenStyleList, ScreenStyleList>();
+
+builder.Services.AddScoped<IScreenStyleManager, ScreenStyleManager>();
+
+builder.Services.AddScoped<IEditDate, EditDate>();
+
+builder.Services.AddScoped<IActionOperations, ActionOperations>();
+
+builder.Services.AddScoped<IFocusedClaim, FocusedClaim>();
+
+builder.Services.AddScoped<IHistorySettings, HistorySettings>();
+
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+await builder.Build().RunAsync();
